fa571d40d16b4853bd89cad8ee542ced0c5a7aba
Marcin Wachacki <mawachacki@gmail.com>
2022-03-20 17:13:51 +0100

    Loading of variables was fixed

--- config/default.py
+++ config/default.py
@@ -6,3 +6,6 @@ SECRET_KEY = os.environ.get('FLASK_SECRET_KEY')
 
 # Root project directory
 BASEDIR = Path(__file__).parent.absolute() / '..'
+
+REDIS_HOST = os.environ.get('REDIS_HOST')
+REDIS_PORT = os.environ.get('REDIS_PORT')
